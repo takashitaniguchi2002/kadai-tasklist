c34256d604caad3c750d874c14038fd895210e6d
takashi.taniguchi <tks2002@icloud.com>
2021-09-29 11:30:22 +0000

    投稿とユーザーの一対多の関係なし

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,22 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = Task.all
+    end 
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -54,5 +58,6 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
 
 end
