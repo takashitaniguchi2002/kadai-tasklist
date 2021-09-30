b1a71ed5311ade64878f1da13cd5d8a03dcf618e
takashi.taniguchi <tks2002@icloud.com>
2021-09-30 06:37:56 +0000

    完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index]
   def index
   end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
+  
+  
   def new
     @user = User.new
   end
