5c628bcdf431d1e02ddc7a591baf79d919515188
takashi.taniguchi <tks2002@icloud.com>
2021-09-30 05:57:30 +0000

    ユーザーログイン後は、root（/）へ遷移し、ログインユーザーが登録したタスクを一覧表示できる前

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy, :show]
   
   def index
     if logged_in?
-      @tasks = Task.all
+      @tasks = current_user.tasks
     end 
   end
 
@@ -59,5 +60,11 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 
 end
