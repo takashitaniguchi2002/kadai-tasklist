b1c69697430a4bc5e256c4aeeabb41d89b2a98a1
takashi.taniguchi <tks2002@icloud.com>
2021-10-02 09:46:09 +0000

    修正１

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,10 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:destroy, :show]
+  before_action :correct_user, only: [:destroy, :show, :edit, :update]
   
   def index
-    if logged_in?
       @tasks = current_user.tasks
-    end 
   end
 
   def show
